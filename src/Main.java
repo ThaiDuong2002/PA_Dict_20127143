27d14b0e99586794bbf53662782d04182a090574
ThaiDuong2002 <105373894+ThaiDuong2002@users.noreply.github.com>
2022-11-15 10:24:14 +0700

    feat: update .gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 /**
-*   
-*   Created by ${USER}
-*   Date ${DATE} - ${TIME}
-*   Description: ...
-*/public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+ * Created by ${USER}
+ * Date ${DATE} - ${TIME}
+ * Description: ...
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
 }
