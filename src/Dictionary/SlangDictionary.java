caaa32bf7950c54a7a6db6309de80df78538fc55
ThaiDuong2002 <105373894+ThaiDuong2002@users.noreply.github.com>
2022-11-16 10:45:56 +0700

    feat(SlangDictionary.java): find by slang, find by definition, show search history, add new slang.

--- src/Dictionary/SlangDictionary.java
+++ src/Dictionary/SlangDictionary.java
@@ -12,6 +12,7 @@ import java.util.*;
 public class SlangDictionary {
 	private static final Hashtable<String, List<String>> dictionary = new Hashtable<>();
 	private static final List<String> historySearch = new ArrayList<>();
+	Scanner input;
 	
 	public void getData() {
 		try {
@@ -24,14 +25,48 @@ public class SlangDictionary {
 				}
 			}
 			br.close();
-			Enumeration<String> e = dictionary.keys();
-			
-			while (e.hasMoreElements()) {
-				String element = e.nextElement();
-				System.out.println(element + ": " + dictionary.get(element));
-			}
 		} catch (Exception ex) {
 			System.out.println("Error: " + ex.getMessage());
+		}
+	}
+	
+	public List<String> findBySlangWord(String word) {
+		historySearch.add(word);
+		word = word.toUpperCase();
+		return dictionary.get(word);
+	}
+	
+	public List<String> findByDefinition(String word) {
+		historySearch.add(word);
+		List<String> slangList = new ArrayList<>();
+		for (String s : dictionary.keySet()) {
+			if (dictionary.get(s).contains(word)) {
+				slangList.add(s);
+			}
+		}
+		return slangList;
+	}
+	
+	public List<String> getHistorySearch() {
+		return historySearch;
+	}
+	
+	public boolean addSlangWord(String slang, List<String> definitions) {
+		if (dictionary.containsKey(slang)) {
+			System.out.println("Do you want to overwrite (Y/N): ");
+			String confirm = input.nextLine();
+			if (confirm.equals("Y") || confirm.equals("y")) {
+				dictionary.put(slang, definitions);
+				return true;
+			} else {
+				List<String> temp = dictionary.get(slang);
+				temp.addAll(definitions);
+				dictionary.put(slang, temp);
+				return true;
+			}
+		} else {
+			dictionary.put(slang, definitions);
+			return true;
 		}
 	}
 }
